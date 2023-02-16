001ef1e14a7655d8ed81d579ee112f52e3695cc4
Angel Avellaneda <116303431+Avellaneda810@users.noreply.github.com>
2023-02-16 13:14:20 -0300

    agregacion de spring al back end

---
+++ src/main/java/com/portafolio/agav/AngavApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.agav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portafolio/agav/AngavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.agav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
