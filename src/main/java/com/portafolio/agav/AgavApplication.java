f2a6eaed4bff1787a4781c0a691a1e8f3f50bbf1
Angel Avellaneda <116303431+Avellaneda810@users.noreply.github.com>
2023-02-16 13:48:46 -0300

    instalacion de spring nueva version

--- src/main/java/com/portafolio/agav/AgavApplication.java
+++ src/main/java/com/portafolio/agav/AgavApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AngavApplication {
+public class AgavApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AngavApplication.class, args);
+		SpringApplication.run(AgavApplication.class, args);
 	}
 
 }
